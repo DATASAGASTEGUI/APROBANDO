a339ee9894bc56bb67667bdf0882eed169d2c010
DATASAGASTEGUI <datasagamadrid@gmail.com>
2024-07-01 19:32:51 +0200

    FOTO 1

---
+++ src/misrc/Principal.java
@@ -0,0 +1,22 @@
+package misrc;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        
+        
+    }
+    
+}
+
+/*
+1. CREAR UN CONTROL DE VERSION PARA MI PROYECTO: Team,Git,Initialize Repository...
+
+2. CONFIGURAR CREDENCIALES: BD NOMBRE_PROYECTO,Git,Repository,Open Configuration
+
+[user]
+	name = DATASAGASTEGUI
+	email = datasagamadrid@gmail.com
+	password = XXXXXXXXXXXXXXXXXXXXXXX
+*/
